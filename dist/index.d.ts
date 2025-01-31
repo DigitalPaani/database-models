18120368d6c095fc8496ad8f284759757d66e6fb
Piyush-DP <piyush.negi@digitalpaani.com>
2025-01-31 22:06:52 +0530

    export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,7 +4,8 @@ import { SensorSettingsModel, ISensorSetting } from "./models/sensorSettingModel
 import { PlcModel, IPlcData } from "./models/plcModel";
 import { TriggerModel, ITriggerDocument } from "./models/trigger.model";
 import { FormulaEventModel, IFormulaEvents } from "./models/formulaEvents";
+import { CompanyModel } from "./models/CompanyModel";
 import { ConnectToDatabase } from "./mongoConnection";
 export type { IFormula, IHyperFormula, ISensorSetting, IPlcData, ITriggerDocument, IFormulaEvents, };
-export { ConnectToDatabase, FormulasModel, HyperFormulaModel, SensorSettingsModel, PlcModel, TriggerModel, FormulaEventModel, };
+export { ConnectToDatabase, FormulasModel, HyperFormulaModel, SensorSettingsModel, PlcModel, TriggerModel, FormulaEventModel, CompanyModel };
 //# sourceMappingURL=index.d.ts.map
