e7887057d55fa433478fc2ee4ffd7a3bca6a3c66
Piyush-DP <piyush.negi@digitalpaani.com>
2025-01-31 15:10:13 +0530

    company model

---
+++ src/models/CompanyModel.ts
@@ -0,0 +1,41 @@
+    const mongoose = require('mongoose');
+
+import { Document, Schema } from "mongoose";
+
+interface Contact {
+  fullName: string;
+  phoneNumber: string;
+  emailId: string;
+  designation: string;
+  comments?:string
+}
+
+interface Company extends Document {
+  companyName: string;
+  companyDescription?: string;
+  address: string;
+  tags?: string[];
+  contacts: Contact[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const contactSchema = new Schema<Contact>({
+  fullName: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  emailId: { type: String, required: true },
+  designation: { type: String, required: true },
+  comments: { type: String, required: false },
+});
+
+const companySchema = new Schema<Company>({
+  companyName: { type: String, required: true },
+  companyDescription: { type: String, required: false },
+  address: { type: String, required: true },
+  tags: { type: [String], required: false },
+  contacts: { type: [contactSchema], required: true }, // Array of contacts
+}, { timestamps: true });
+
+const CompanyModel = mongoose.model('Company', companySchema);
+
+export {CompanyModel};
