9186929c1782598793c235ac57eed7946e6c6fa9
Piyush-DP <piyush.negi@digitalpaani.com>
2025-01-31 15:19:54 +0530

    enums

--- dist/models/CompanyModel.js
+++ dist/models/CompanyModel.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CompanyModel = void 0;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const mongoose_1 = require("mongoose");
 const contactSchema = new mongoose_1.Schema({
     fullName: { type: String, required: true },
@@ -14,8 +14,12 @@ const companySchema = new mongoose_1.Schema({
     companyName: { type: String, required: true },
     companyDescription: { type: String, required: false },
     address: { type: String, required: true },
-    tags: { type: [String], required: false },
+    tags: {
+        type: [String],
+        enum: ["sensor", "electrical panel"], // Restrict values
+        required: false,
+    },
     contacts: { type: [contactSchema], required: true }, // Array of contacts
 }, { timestamps: true });
-const CompanyModel = mongoose.model('Company', companySchema);
+const CompanyModel = mongoose.model("Company", companySchema);
 exports.CompanyModel = CompanyModel;
--- src/models/CompanyModel.ts
+++ src/models/CompanyModel.ts
@@ -1,4 +1,4 @@
-    const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 import { Document, Schema } from "mongoose";
 
@@ -7,7 +7,7 @@ interface Contact {
   phoneNumber: string;
   emailId: string;
   designation: string;
-  comments?:string
+  comments?: string;
 }
 
 interface Company extends Document {
@@ -28,14 +28,21 @@ const contactSchema = new Schema<Contact>({
   comments: { type: String, required: false },
 });
 
-const companySchema = new Schema<Company>({
-  companyName: { type: String, required: true },
-  companyDescription: { type: String, required: false },
-  address: { type: String, required: true },
-  tags: { type: [String], required: false },
-  contacts: { type: [contactSchema], required: true }, // Array of contacts
-}, { timestamps: true });
+const companySchema = new Schema<Company>(
+  {
+    companyName: { type: String, required: true },
+    companyDescription: { type: String, required: false },
+    address: { type: String, required: true },
+    tags: {
+      type: [String],
+      enum: ["sensor", "electrical panel"], // Restrict values
+      required: false,
+    },
+    contacts: { type: [contactSchema], required: true }, // Array of contacts
+  },
+  { timestamps: true }
+);
 
-const CompanyModel = mongoose.model('Company', companySchema);
+const CompanyModel = mongoose.model("Company", companySchema);
 
-export {CompanyModel};
+export { CompanyModel };
