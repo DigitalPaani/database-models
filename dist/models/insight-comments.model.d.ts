44a6a0980ad45490841a6e071f4df11bbacc6943
remansharma-digitalpaani <reman.sharma@digitalpaani.com>
2025-04-30 16:17:18 +0530

    added dist

--- dist/models/insight-comments.model.d.ts
+++ dist/models/insight-comments.model.d.ts
@@ -1,9 +1,10 @@
 import { Model } from "mongoose";
 import type { Document } from "mongoose";
 import { Types } from "mongoose";
+import type { INewUser } from "./newUserModel";
 interface IInsightComment extends Document {
     insightId: Types.ObjectId;
-    senderId: Types.ObjectId;
+    senderId: Types.ObjectId | INewUser;
     comment: string;
     isArchived: boolean;
     createdAt?: Date;
--- src/models/insight-comments.model.ts
+++ src/models/insight-comments.model.ts
@@ -1,11 +1,12 @@
 import mongoose, { Model, Schema } from "mongoose";
 import type { Document } from "mongoose";
 import { Types } from "mongoose";
+import type { INewUser } from "./newUserModel";
 
 
 interface IInsightComment extends Document {
     insightId: Types.ObjectId;
-    senderId: Types.ObjectId;
+    senderId: Types.ObjectId | INewUser;
     comment: string
     isArchived: boolean;
     createdAt?: Date;
