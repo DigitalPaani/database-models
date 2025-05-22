599af72769817d0aa4ee8d0d3e8b3f885af25ea4
remansharma-digitalpaani <reman.sharma@digitalpaani.com>
2025-05-22 11:58:14 +0530

    added whatsapp incoming events

---
+++ src/models/whatsappIncomingEvents.model.ts
@@ -0,0 +1,63 @@
+import type { Document, Model } from "mongoose";
+import mongoose, { Schema } from "mongoose";
+
+interface IWhatsappIncomingEvent extends Document {
+    profileName: string;
+    messageServiceId: string;
+    messageType: string;
+    from: string;
+    to: string;
+    body: string;
+    smsStatus: string;
+    twilioResponse: any;
+}
+
+const twilioCallSchema = new Schema(
+  {
+    profileName: {
+        type: String,
+        required: false,  
+    },
+    messageServiceId: {
+      type: String,
+      required: false,
+    },
+    messageType: {
+        type: String,
+        required: false,
+    },
+    from: {
+        type: String,
+        required: false,
+    },
+    to: {
+        type: String,
+        required: false,
+    },
+    body: {
+        type: String,
+        required: false,
+    },
+    smsStatus: {
+        type: String,
+        required: false,
+    },
+    twilioResponse: {
+        type: Schema.Types.Mixed,
+        required: false,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const WhatsappIncomingEventModel: Model<IWhatsappIncomingEvent> = mongoose.model<IWhatsappIncomingEvent>(
+  "whatsappIncomingMessagesEvents",
+  twilioCallSchema,
+  "whatsappIncomingMessagesEvents"
+);
+
+export { WhatsappIncomingEventModel, IWhatsappIncomingEvent };
+
+
