f912aaeaccb7229f7f6a8f04d7e248fb4496467d
VibhanshuSrivastav <kvibhanshu6@gmail.com>
2025-05-27 21:21:23 +0530

    feat: added abbr in manualSensorCategoryConst

--- src/constants/manualSensorsCategoryConst.ts
+++ src/constants/manualSensorsCategoryConst.ts
@@ -2,29 +2,36 @@ export const manualCategoryList = [
   {
     name: "Water Quality",
     key: "WATER_QUALITY",
+    abbr: "WQ",
   },
   {
     name: "Lab Test",
     key: "LAB_TEST",
+    abbr: "LT",
   },
   {
     name: "Stream",
     key: "STREAM",
+    abbr: "STR",
   },
   {
     name: "Steam",
     key: "STEAM",
+    abbr: "ST",
   },
   {
     name: "Flow",
     key: "FLOW",
+    abbr: "FL",
   },
   {
     name: "Pressure",
     key: "PRESSURE",
+    abbr: "PR",
   },
   {
     name: "Billing",
     key: "BILLING",
+    abbr: "BI",
   }
 ]
