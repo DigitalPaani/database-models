bb282f187ae34d54979b22edc76ac687cd222881
remansharma-digitalpaani <reman.sharma@digitalpaani.com>
2025-07-10 14:03:12 +0530

    added dist

--- dist/index.js
+++ dist/index.js
@@ -37,7 +37,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.InsightComponentModel = exports.InsightsTemplateModel = exports.FormulaAuditsModel = exports.FlocImageModel = exports.FlocSampleModel = exports.TaskLogModel = exports.TaskCommentModel = exports.LayoutPathModel = exports.SidebarLayoutItemsModel = exports.LayoutEquipmentModel = exports.LayoutModel = exports.SensorsModel = exports.EquipmentQuestionsModel = exports.AnswerSchemaModel = exports.BatchSchemaModel = exports.QuestionSchemaModel = exports.ObsoleteTaskModel = exports.mongooseDatabaseModels = exports.SensorModel = exports.CompanyModel = exports.commonConstants = exports.NewUserModel = exports.CommunicationComponentsModel = exports.TaskComponentsModel = exports.TriggerComponentModel = exports.Types = exports.WhatsappTemplateModel = exports.EmailLogModel = exports.SmsLogModel = exports.UserGroupModel = exports.StoreItemsModel = exports.Store = exports.WhatsappLogModel = exports.CallLogModel = exports.TwilioCallModel = exports.AttachmentModel = exports.TaskModel = exports.WorkflowModel = exports.RosterSchedulesModel = exports.RosterManagementModel = exports.SkillManagementModel = exports.TaskTemplateModel = exports.FormulaEventModel = exports.TriggerModel = exports.PlcModel = exports.SensorSettingsModel = exports.HyperFormulaModel = exports.PlantModel = exports.FormulasModel = exports.ConnectToDatabase = void 0;
-exports.NotificationComponentsModel = exports.WorkspaceModel = exports.BulkEntryReportModel = exports.NotificationModel = exports.EquipmentComponentModel = exports.BidirectionalLogsModel = exports.BidirectionalModel = exports.DataLoggerModel = exports.ManualSensorModel = exports.WhatsappAiCommunicationModel = exports.WhatsappIncomingEventModel = exports.InsightCommentModel = exports.InsightModel = void 0;
+exports.NotificationComponentsModel = exports.WorkspaceModel = exports.BulkEntryReportModel = exports.GeneralCommunicationModel = exports.NotificationModel = exports.EquipmentComponentModel = exports.BidirectionalLogsModel = exports.BidirectionalModel = exports.DataLoggerModel = exports.ManualSensorModel = exports.WhatsappAiCommunicationModel = exports.WhatsappIncomingEventModel = exports.InsightCommentModel = exports.InsightModel = void 0;
 const formulaModel_1 = require("./models/formulaModel");
 Object.defineProperty(exports, "FormulasModel", { enumerable: true, get: function () { return formulaModel_1.FormulasModel; } });
 const hyperFormulaModel_1 = require("./models/hyperFormulaModel");
@@ -156,17 +156,14 @@ const whatsapp_ai_communication_model_1 = require("./models/whatsapp-ai-communic
 Object.defineProperty(exports, "WhatsappAiCommunicationModel", { enumerable: true, get: function () { return whatsapp_ai_communication_model_1.WhatsappAiCommunicationModel; } });
 const manual_sensors_template_model_1 = require("./models/manual-sensors-template.model");
 Object.defineProperty(exports, "ManualSensorModel", { enumerable: true, get: function () { return manual_sensors_template_model_1.ManualSensorModel; } });
-<<<<<<< HEAD
 const general_communication_model_1 = require("./models/general-communication.model");
 Object.defineProperty(exports, "GeneralCommunicationModel", { enumerable: true, get: function () { return general_communication_model_1.GeneralCommunicationModel; } });
-=======
 const data_input_bulk_entry_report_1 = require("./models/data-input-bulk-entry-report");
 Object.defineProperty(exports, "BulkEntryReportModel", { enumerable: true, get: function () { return data_input_bulk_entry_report_1.BulkEntryReportModel; } });
 const userGroupModel_1 = require("./models/userGroupModel");
 Object.defineProperty(exports, "UserGroupModel", { enumerable: true, get: function () { return userGroupModel_1.UserGroupModel; } });
 const notificationComponents_model_1 = require("./models/notificationComponents.model");
 Object.defineProperty(exports, "NotificationComponentsModel", { enumerable: true, get: function () { return notificationComponents_model_1.NotificationComponentsModel; } });
->>>>>>> a986b462877eb14652903ba2f39539ae6075c502
 // Assign mongoose to mongooseDatabaseModels
 const mongooseDatabaseModels = mongoose_1.default;
 exports.mongooseDatabaseModels = mongooseDatabaseModels;
