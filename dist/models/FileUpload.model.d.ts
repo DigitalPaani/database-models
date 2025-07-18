748a7a229b8509fcb1cb24ae761b8c95c397c117
Piyush-DP <piyush.negi@digitalpaani.com>
2025-07-18 20:50:48 +0530

    file Management

---
+++ dist/models/FileUpload.model.d.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+interface IFileUpload extends Document {
+    _id?: string;
+    plantId: string;
+    uploaderId: string;
+    fileName: string;
+    description?: string;
+    category: string;
+    subCategory: string;
+    s3Key: string;
+    fileType: string;
+    fileSize: number;
+    etag?: string;
+    uploadedAt: string;
+    deletedAt?: string;
+}
+declare const FileManagementUploadModel: mongoose.Model<IFileUpload, {}, {}, {}, mongoose.Document<unknown, {}, IFileUpload> & IFileUpload & Required<{
+    _id: string;
+}> & {
+    __v: number;
+}, any>;
+export { IFileUpload, FileManagementUploadModel };
+//# sourceMappingURL=FileUpload.model.d.ts.map
---
+++ src/models/FileUpload.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IFileUpload extends Document {
+  _id?: string;
+  plantId: string;
+  uploaderId: string;
+  fileName: string;
+  description?: string;
+  category: string;
+  subCategory: string;
+  s3Key: string;
+  fileType: string;
+  fileSize: number;
+  etag?: string;
+  uploadedAt: string;
+  deletedAt?: string;
+}
+
+const uploadSchema = new Schema(
+  {
+    plantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Plant",
+      required: true,
+    },
+    uploaderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    fileName: { type: String, required: true },
+    description: String,
+    s3Key: { type: String, required: true },
+    fileType: String,
+    fileSize: Number,
+    etag: String,
+    uploadedAt: { type: Date, default: Date.now },
+    deletedAt: Date,
+  },
+  { timestamps: true }
+);
+
+const FileManagementUploadModel = mongoose.model<IFileUpload>(
+  "FileManagementUploadDetails",
+  uploadSchema,
+  "FileManagementUploadDetails"
+);
+
+export { IFileUpload, FileManagementUploadModel };
