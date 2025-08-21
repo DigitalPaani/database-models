092ce7e94587087b784d4cdef8915fabb2aa5a47
remansharma-digitalpaani <reman.sharma@digitalpaani.com>
2025-08-21 16:52:10 +0530

    fixes

--- dist/constants/logbook-templates.constants.d.ts
+++ dist/constants/logbook-templates.constants.d.ts
@@ -1,6 +1,26 @@
 declare const _default: {
     LOGBOOK_TEMPLATE_TYPES_ENUMS: string[];
     LOGBOOK_CATEGORY_TYPES_ENUMS: string[];
+    LOGBOOK_TEMPLATE_TYPES: {
+        logbook: {
+            label: string;
+            value: string;
+        };
+        inventory: {
+            label: string;
+            value: string;
+        };
+    };
+    CATEGORY_TYPES: {
+        logbook: {
+            label: string;
+            value: string;
+        };
+        inventory: {
+            label: string;
+            value: string;
+        };
+    };
 };
 export default _default;
 //# sourceMappingURL=logbook-templates.constants.d.ts.map
