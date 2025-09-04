adccdf21c9421eded7e103e1640abcd6f7aa9e6f
rk8126 <rajan.kashyap814@gmail.com>
2025-09-04 16:04:21 +0530

    fix: check partial ttl

---
+++ src/models/anomaly-data.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+interface IAnomalyData extends Document {
+  triggerId: Types.ObjectId;
+  formulaId: Types.ObjectId;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const anomalyDataSchema = new Schema<IAnomalyData>(
+  {
+    triggerId: {
+      type: Schema.Types.ObjectId,
+      ref: "triggers",
+      required: true,
+    },
+    formulaId: {
+      type: Schema.Types.ObjectId,
+      ref: "formulas",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+anomalyDataSchema.index(
+  { createdAt: 1 },
+  {
+    expireAfterSeconds: 120,
+    partialFilterExpression: { formulaId: { $exists: true } },
+  }
+);
+
+const AnomalyDataModel = mongoose.model<IAnomalyData>(
+  "anomalyData",
+  anomalyDataSchema,
+  "anomalyData"
+);
+
+export { AnomalyDataModel, anomalyDataSchema };
