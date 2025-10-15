ca9c7f1071ac6d146d210c4f3b5c432a714d02df
Piyush-DP <piyush.negi@digitalpaani.com>
2025-10-15 16:34:58 +0530

    typo fix

--- src/models/EscalationFlow.model.ts
+++ src/models/EscalationFlow.model.ts
@@ -29,7 +29,7 @@ const escalationStepSchema = new Schema(
 
 // Interface
 interface IEscalationFlow extends Document {
-  attendenceManager: Types.ObjectId[];
+  attendanceManager: Types.ObjectId[];
   rosterManager: Types.ObjectId[];
   operator: Types.ObjectId[];
   userGroup: Types.ObjectId;
@@ -41,7 +41,7 @@ interface IEscalationFlow extends Document {
 // Main schema
 const escalationFlowSchema = new Schema<IEscalationFlow>(
   {
-    attendenceManager: [
+    attendanceManager: [
       {
         type: Schema.Types.ObjectId,
         ref:"NewUser",
