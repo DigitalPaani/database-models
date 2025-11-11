4c453e9ce384b9a80ae874aa3f6cb6f85a2182a1
remansharma-digitalpaani <reman.sharma@digitalpaani.com>
2025-11-11 17:45:47 +0530

    Communication/content template ids (#90)
    * added dist
    
    * added dist

--- dist/constants/communication.constants.d.ts
+++ dist/constants/communication.constants.d.ts
@@ -1,5 +1,16 @@
 declare const _default: {
     DELIVERY_STATUS_ENUMS: string[];
+    CONTENT_TEMPLATE_IDS: {
+        new_insight: {
+            template_id: string;
+        };
+        account_pending_updates: {
+            template_id: string;
+        };
+        dashboard_report_ready: {
+            template_id: string;
+        };
+    };
 };
 export default _default;
 //# sourceMappingURL=communication.constants.d.ts.map
