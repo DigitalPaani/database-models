8fd4e24cbb3bafa707cec36d30289d2ffa87f541
rk8126 <rajan.kashyap814@gmail.com>
2025-11-17 12:33:56 +0530

    made extra changes

--- dist/constants/user-configuration-logs.constants.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const USER_CONFIG_LOGS_ACTION_ENUM: {
-    CREATED: string;
-    UPDATED: string;
-    DELETED: string;
-};
-//# sourceMappingURL=user-configuration-logs.constants.d.ts.map
